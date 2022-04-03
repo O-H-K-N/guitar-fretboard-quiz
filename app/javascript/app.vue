8476f1bbf42896c49d23f0d79391291921921829
大塚健登 <kent0tsuka1118@gmail.com>
2022-04-03 14:30:42 +0900

    Fix：ヘッダー・フッターをVueで表示されるように変更

---
+++ app/javascript/app.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" class="jumbotron" >
+    <TheHeader class="mb-auto" />
+    <TheFooter class="fixed-bottom" />
+  </div>
+</template>
+
+<script>
+import TheHeader from 'components/TheHeader.vue'
+import TheFooter from 'components/TheFooter.vue'
+
+export default {
+  name: 'App',
+  components: { TheHeader, TheFooter }
+}
+</script>
+
+<style scoped>
+.jumbotron {
+  position: relative;
+  height: 100vh;
+  background-position: right;
+  background-image: linear-gradient(rgba(199, 141, 75, 0.5),rgba(0, 0, 0, 0.5)),url('../assets/images/background.jpg');
+  background-size: cover;
+  border-radius: 0;
+  padding: 0px;
+  margin-bottom: 0px;
+}
+</style>
---
+++ app/javascript/components/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <nav class="navbar navbar-light">
+      <div class="container-sm">
+        <router-link to="/" class="navbar-brand d-flex align-items-center">
+          <span class="text-light">{{ app_name }}</span>
+        </router-link>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TheHeader",
+  data() {
+    return {
+      app_name: "Quiz Guitar Chord"
+    }
+  },
+}
+</script>
+
+<style scoped>
+header{
+  background-color: rgba(245, 245, 220, 0.4);
+  box-shadow: 0px 2px 4px #ddebf3, 0px -2px 4px #ddebf3;
+}
+</style>
