fc660d88824e93b08492c14751c32cb8172d3007
大塚健登 <kent0tsuka1118@gmail.com>
2022-04-03 22:17:47 +0900

    Add：クイズの表示・全てのクイズ解答後に正解数が表示されるよう実装

--- app/javascript/pages/quiz/components/EasyQuizModel.vue
+++ app/javascript/pages/quiz/components/EasyQuizModel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="quiz-easy">
+  <div id="easy-quiz-model">
     <div class="modal">
       <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
         <div class="modal-content">
@@ -13,24 +13,34 @@
           </template>
           <template v-else>
             <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">aaa</h5>
+            <h5 class="modal-title" :id="'question-label-' + questionIndex+1">第 {{ (questionIndex+1) }} 問　{{ currentQuestion.question }}</h5>
             </div>
             <div class="modal-body">
-              <p>aaaa</p>
+              <button
+                type="button"
+                class="btn btn-primary btn-lg btn-block text-left"
+                v-for="(option, key) in currentQuestion.options"
+                :key="key"
+                @click="addAnswer(key)"
+              >
+                {{ (key+1) }}. {{ option }}
+              </button>
             </div>
             <div class="modal-footer">
               <button @click="handleOpenQuizConfirmationModel" class="btn btn-secondary" data-dismiss="modal">CLOSE</button>
             </div>
+            <transition name="fade">
+              <QuizConfirmationModel
+                v-if="isVisibleCloseQuizConfirmationModel"
+                @close-quiz="handleCloseCloseQuizModel"
+                @close-confirmation="handleCloseQuizConfirmationModel"
+              />
+            </transition>
           </template>
         </div>
       </div>
     </div>
     <div class="modal-backdrop show"></div>
-    <QuizConfirmationModel
-      v-if="isVisibleCloseQuizConfirmationModel"
-      @close-quiz="handleCloseCloseQuizModel"
-      @close-confirmation="handleCloseQuizConfirmationModel"
-    />
   </div>
 </template>
 
@@ -42,8 +52,47 @@ export default {
   components: { QuizConfirmationModel },
   data() {
     return {
+      answers: [],
+      questionIndex: 0,
+      questions: [
+        {
+          question: '「A」の音は？',
+          options: [
+              '６弦開放',
+              '５弦開放',
+              '４弦開放',
+              '３弦開放',
+          ],
+          answer: 1
+        },
+        {
+          question: '「C」の音は？',
+          options: [
+              '６弦４フレット',
+              '５弦３フレット',
+              '４弦２フレット',
+              '３弦１フレット',
+          ],
+          answer: 1
+      },
+        {
+          question: '「D」の音は？',
+          options: [
+              '３弦開放',
+              '２限開放',
+              '２弦３フレット',
+              '１弦３フレット',
+          ],
+          answer: 2
+        }
+      ],
       startFlag: false,
       isVisibleCloseQuizConfirmationModel: false,
+    }
+  },
+  computed: {
+    currentQuestion() {
+      return this.questions[this.questionIndex];
     }
   },
   methods: {
@@ -55,6 +104,23 @@ export default {
     },
     handleCloseCloseQuizModel() {
       this.$emit('close-quiz')
+    },
+    // 正答数カウンター
+    addAnswer(key) {
+      this.answers.push(key);
+      if(this.questions.length == this.answers.length) {
+        var correctCount = 0;
+        for(var i in this.answers) {
+          var answer = this.answers[i];
+          if(answer == this.questions[i].answer) {
+              correctCount++;
+          }
+        }
+        alert(this.questionIndex+1 + '問中' + correctCount +'問正解です！');
+        this.$emit('close-quiz')
+      } else {
+        this.questionIndex++;
+      }
     },
   }
 }
