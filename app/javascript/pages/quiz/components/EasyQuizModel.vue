0b66cf5ba0c5104955db778d42e8fc837db4ad84
大塚健登 <kent0tsuka1118@gmail.com>
2022-04-03 20:32:12 +0900

    Add：Quiz用のページがポップアップで表示されるように設定・閉じるボタンも実装

---
+++ app/javascript/pages/quiz/components/EasyQuizModel.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="quiz-easy">
+    <div class="modal">
+      <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <template
+            v-if="!startFlag"
+          >
+            <div class="modal-body text-center">
+              <button @click="startFlag = true"  class="btn btn-success" data-bs-toggle="modal" data-dismiss="modal">START</button>
+              <button @click="handleCloseCloseQuizModel" class="btn btn-secondary" data-dismiss="modal">CLOSE</button>
+            </div>
+          </template>
+          <template v-else>
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">aaa</h5>
+            </div>
+            <div class="modal-body">
+              <p>aaaa</p>
+            </div>
+            <div class="modal-footer">
+              <button @click="handleOpenQuizConfirmationModel" class="btn btn-secondary" data-dismiss="modal">CLOSE</button>
+            </div>
+          </template>
+        </div>
+      </div>
+    </div>
+    <div class="modal-backdrop show"></div>
+    <QuizConfirmationModel
+      v-if="isVisibleCloseQuizConfirmationModel"
+      @close-quiz="handleCloseCloseQuizModel"
+      @close-confirmation="handleCloseQuizConfirmationModel"
+    />
+  </div>
+</template>
+
+<script>
+import QuizConfirmationModel from './QuizConfirmationModel.vue'
+
+export default {
+  name: "QuizEasy",
+  components: { QuizConfirmationModel },
+  data() {
+    return {
+      startFlag: false,
+      isVisibleCloseQuizConfirmationModel: false,
+    }
+  },
+  methods: {
+    handleOpenQuizConfirmationModel() {
+      this.isVisibleCloseQuizConfirmationModel = true;
+    },
+    handleCloseQuizConfirmationModel() {
+      this.isVisibleCloseQuizConfirmationModel = false;
+    },
+    handleCloseCloseQuizModel() {
+      this.$emit('close-quiz')
+    },
+  }
+}
+</script>
+
+<style scoped>
+.modal{
+  display: block;
+}
+</style>
--- app/javascript/pages/top/index.vue
+++ app/javascript/pages/top/index.vue
@@ -3,24 +3,48 @@
     <div class='top-inner-text text-center'>
       <p class='title-logo text-light'>{{ app_name }}</p>
       <div class='quiz-select-field neumorphism'>
-        <button class="btn btn-outline-light font-weight-bold filter-none px-3 m-3" type="button">{{ level1}} </button><br>
+        <button
+          @click="handleOpenEasyQuizModel()"
+          class="btn btn-outline-light font-weight-bold filter-none px-3 m-3"
+          type="button">
+            {{ level1}}
+          </button><br>
         <button class="btn btn-outline-light font-weight-bold px-3 m-3" type="button">{{ level2 }}</button><br>
         <button class="btn btn-outline-light font-weight-bold px-3 m-3" type="button">{{ level3 }}</button>
       </div>
     </div>
+    <transition name="fade">
+      <EasyQuizModel
+        v-if="isVisibleEasyQuizModel"
+        @close-quiz="handleCloseEasyQuizModel"
+      />
+    </transition>
   </div>
 </template>
 
 <script>
+import EasyQuizModel from '../quiz/components/EasyQuizModel.vue'
+
 export default {
   name: "TopIndex",
+  components: { EasyQuizModel },
   data() {
     return {
       app_name: "Quiz Guitar Chord",
       level1: "E A S Y",
       level2: "M I D D L E",
-      level3: "H I G H"
+      level3: "H I G H",
+
+      isVisibleEasyQuizModel: false,
     }
+  },
+  methods: {
+    handleOpenEasyQuizModel() {
+      this.isVisibleEasyQuizModel = true;
+    },
+    handleCloseEasyQuizModel() {
+      this.isVisibleEasyQuizModel = false;
+    },
   }
 }
 </script>
@@ -49,5 +73,13 @@ export default {
   background-color: rgba(95, 50, 8, 0.5);
   border: medium solid white;
   box-shadow: 2px 3px 4px #a3adb3, -2px -2px 4px #ddebf3;
+}
+
+.fade-enter-active, .fade-leave-active {
+  will-change: opacity;
+  transition: opacity 800ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0
 }
 </style>
