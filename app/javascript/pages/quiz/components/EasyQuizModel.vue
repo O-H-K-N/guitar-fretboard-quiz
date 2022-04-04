a1b5a259584e29b7f75a21154c843f2cb2e2d2d9
大塚健登 <kent0tsuka1118@gmail.com>
2022-04-04 21:54:11 +0900

    Add：クイズがシャッフルされて出題されるように実装

--- app/javascript/pages/quiz/components/EasyQuizModel.vue
+++ app/javascript/pages/quiz/components/EasyQuizModel.vue
@@ -7,7 +7,7 @@
             v-if="!startFlag"
           >
             <div class="modal-body text-center">
-              <button @click="startFlag = true"  class="btn btn-success" data-bs-toggle="modal" data-dismiss="modal">始める</button>
+              <button @click="quizShuffle(quizzes);" class="btn btn-success" data-bs-toggle="modal" data-dismiss="modal">始める</button>
               <button @click="handleCloseQuizModel" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
             </div>
           </template>
@@ -105,6 +105,19 @@ export default {
       this.$axios.get("quizzes")
         .then(res => this.quizzes = res.data)
         .catch(err => console.log(err.status));
+    },
+    // this.quizzesの中身をシャッフルするメソッド
+    quizShuffle(array) {
+      this.startFlag = true
+      for(var i = (array.length - 1); 0 < i; i--){
+        // 0〜(i+1)の範囲で値を取得
+        var r = Math.floor(Math.random() * (i + 1));
+        // 要素の並び替えを実行
+        var tmp = array[i];
+        array[i] = array[r];
+        array[r] = tmp;
+      }
+      return array;
     },
     handleOpenQuizConfirmationModel() {
       this.isVisibleQuizConfirmationModel = true;
