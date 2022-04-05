8097486ff6cb84edd37ec8c39c6da03855d8918c
大塚健登 <kent0tsuka1118@gmail.com>
2022-04-06 00:03:18 +0900

    Fix：metaタグを使ってOGPの設定を修正・アイコンの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site:'ギター指板クイズ',
+      title: 'クイズで指板の位置を音名で覚えよう！',
+      reverse: true,
+      charset: 'utf-8',
+      description: '音名をギターので答えるクイズアプリです。',
+      keywords: 'ギター, 指板, コード, フレット, アコースティックギター, アコギ',
+      canonical: request.original_url,
+      separator: '|',
+      icon: [
+        { href: image_url('favicon.png')},
+        { href: image_url('favicon.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
+      ],
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+    }
+  end
 end
