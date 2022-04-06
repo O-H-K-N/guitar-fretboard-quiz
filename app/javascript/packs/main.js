5afb7be1a5605d105e1d02c0a2a3e7c524593ff4
大塚健登 <kent0tsuka1118@gmail.com>
2022-04-06 17:16:50 +0900

    Add：FontAwesomeIconの導入

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -2,10 +2,17 @@ import Vue from 'vue'
 import App from '../app.vue'
 import router from '../router/index.js'
 import axios from '../plugins/axios'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import 'bootstrap/dist/css/bootstrap.css'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
+library.add(fas, far, fab)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
